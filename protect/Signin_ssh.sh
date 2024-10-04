f246b21ec045d9f677d96270c48eebf7cb627292
Diego Jacobo Martínez <85468901+Djmr5@users.noreply.github.com>
2024-10-03 19:08:50 -0600

    Sign in via ssh

---
+++ protect/Signin_ssh.sh
@@ -0,0 +1,16 @@
+# To signin via ssh to clone or repo or any other action you may create an ssh key and add it to github
+# You can create a ssh key with the following
+  # I encourage using a strong passphrase for maximum security
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+# Then you may add the public contents of the file to github and add the necessary permissions
+cat ~/.ssh/id_rsa.pub
+
+# Finally add the key to the ssh agent on every new terminal session
+ssh-add ~/.ssh/id_rsa
+
+# Now you can verify your connection via:
+ssh -T git@github.com # Hi username! You've successfully authenticated.
+
+# Or clone some repo
+git clone git@github.com:username/repository.git target_folder
